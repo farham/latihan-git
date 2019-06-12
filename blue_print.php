56ae9592b2d0b9ed885e21de8b02920748a8d489
farham <lipi.farham@gmail.com>
2019-06-12 21:41:55 +0700

    Merancang Blue Print Sistem

---
+++ blue_print.php
@@ -0,0 +1,8 @@
+<?php
+echo "1. Merancang Proses Bisnis";
+echo "2. Merancang Desain Database";
+echo "3. Merancang Mockup";
+echo "4. Merancang FrontEnd Script";
+echo "5. Merancang BackEnd Script";
+
+?>
