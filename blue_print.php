f42d70f1338755177b0ba279defbfc1e0d1ccc81
farham <lipi.farham@gmail.com>
2019-06-12 21:48:37 +0700

    Rev. 1 - Tambahan API, Integrasi, Mobile

--- blue_print.php
+++ blue_print.php
@@ -4,5 +4,8 @@ echo "2. Merancang Desain Database";
 echo "3. Merancang Mockup";
 echo "4. Merancang FrontEnd Script";
 echo "5. Merancang BackEnd Script";
+echo "6. Membuat API";
+echo "7. Merancang Integrasi dengan Aplikas Lannya";
+echo "8. Membuat Aplikasi Mobile Android";
 
 ?>
