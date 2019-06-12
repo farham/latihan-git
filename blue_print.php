f316452014c5a8e84dbf91d1eacc54430fe1ca14
farham <lipi.farham@gmail.com>
2019-06-12 22:00:49 +0700

    Rincian Backend Script

--- blue_print.php
+++ blue_print.php
@@ -9,6 +9,8 @@ echo "3. Merancang Mockup";
 echo "3.1 - Mendesain dengan Adobe XD";
 echo "4. Merancang FrontEnd Script";
 echo "5. Merancang BackEnd Script";
+echo "5.1 - Membuat Kerangka Framework";
+echo "5.2 - Melakukan Configruasi Framework";
 echo "6. Membuat API";
 echo "7. Merancang Integrasi dengan Aplikas Lannya";
 echo "8. Membuat Aplikasi Mobile Android";
